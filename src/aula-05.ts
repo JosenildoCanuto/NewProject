905735cffb3280383c5ed7632eae11b3ee01f39c
Josenildo Canuto <josenildo.acanuto@gmail.com>
2023-07-11 13:45:15 -0300

    feat: aula05

---
+++ src/aula-05.ts
@@ -0,0 +1,30 @@
+interface Estado {
+  nome: String
+}
+
+interface Cidade {
+  nome: String
+}
+
+interface Endereco {
+  bairro: String;
+  rua: String;
+  complemento?: String;
+  cidade: Cidade;
+  estado: Estado;
+}
+
+const endereco: Endereco = {
+  bairro: "Farol",
+  rua: "AJBjbasdjfbajsbdasjk",
+  complemento: "sdbfshjdbfsjdbf",
+  cidade: {
+    nome: "Maceió"
+  },
+  estado: {
+    nome: "Alagoas"
+  }
+}
+
+console.log(endereco)
+console.log(endereco.complemento?.substring(3) || "CONSEGUIR")
