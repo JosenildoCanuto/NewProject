46f980d21934fd133c0a09ff16613698cae074c5
Josenildo Canuto <josenildo.acanuto@gmail.com>
2023-07-11 10:36:17 -0300

    feat: aula03

---
+++ src/aula-03.ts
@@ -0,0 +1,15 @@
+interface Pessoa {
+  nome: String,
+  idade: Number,
+  isDeMaior: boolean,
+  teste: any
+}
+
+const pessoa: Pessoa = {
+  nome: "Neto",
+  idade: 18,
+  isDeMaior: true,
+  teste: 'ksbdfsjdbf'
+}
+
+console.log(pessoa)
